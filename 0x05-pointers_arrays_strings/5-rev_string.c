bd82f6f1440f53e1d51dd59abcacc7bd6d867b35
shereenmhussein <shereenmhussein@gmail.com>
2023-10-16 13:10:10 -0700

    s

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,9 +14,9 @@ void rev_string(char *s)
 	char temp;
 
 	/*find string length without null char*/
-	for (l = 0; s[l] != '0'; ++l)
+	for (l = 0; s[l] != '\0'; ++l)
 		;
-	/*swap the string by looping to hlaf the string*/
+	/*swap the string by looping to half the string*/
 	for (i = 0; i < l / 2; i++)
 	{
 		temp = s[i];
