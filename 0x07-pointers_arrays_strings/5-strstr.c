2fc1723f70f09747463b407110fc2f5dd08697f6
shereenmhussein <shereenmhussein@gmail.com>
2023-10-23 12:45:04 -0700

    s

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.>
+
+/**
+ * *_strstr - description
+ * @haystack: string
+ * @needle: pointer
+ *
+ * Return: pointer
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int iqe, jqe;
+
+	for (iqe = 0; haystack[iqe] > '\0'; iqe++)
+	{
+		for (jqe = iqe; haystack[jqe] > '\0' && needle[jqe - iqe] > '\0'; jqe++)
+		{
+			if (haystack[jqe] != needle[jqe - iqe])
+			{
+				break;
+			}
+		}
+		if (needle[jqe - iqe] == '\0')
+		{
+			return (haystack + iqe);
+		}
+	}
+	return (0);
+}
