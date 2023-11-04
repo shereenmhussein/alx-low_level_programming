54d10907ae4d83de11fbe5e0569c58b9eb426666
shereenmhussein <shereenmhussein@gmail.com>
2023-11-04 05:34:56 -0700

    S

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	;
 
 	/*+1 on the size puts the end of string character*/
-	| m = malloc(size * sizeof(*str) + 1);
+	 m = malloc(size * sizeof(*str) + 1);
 
 	if (m == 0)
 		return (NULL);
