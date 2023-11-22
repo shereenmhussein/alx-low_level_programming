65c957a5f8ef860a8ec1bb603cc6414c702a64c4
shereenmhussein <shereenmhussein@gmail.com>
2023-11-22 03:24:48 -0800

    s

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,10 +14,10 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	len = max - min + 1;
-	prt = malloc(sizeof(int) * len);
+	ptr = malloc(sizeof(int) * len);
 	if (!ptr)
 		return (NULL);
 	for (i = 0; i < len; i++)
-		prt[i] = min++;
+		ptr[i] = min++;
 	return (ptr);
 }
