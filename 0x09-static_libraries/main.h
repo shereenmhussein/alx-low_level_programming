6aee3a9976c240fea4cbf9dc04fc9d1f69ab25e8
shereenmhussein <shereenmhussein@gmail.com>
2023-11-22 13:55:26 -0800

    s

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include "main.h"
-
-int main(void)
-{
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
-}
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
