af381f843b6e0003806425b59e54f59c9c167445
shereenmhussein <shereenmhussein@gmail.com>
2023-11-22 12:28:42 -0800

    s

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,7 +2,7 @@
 
 /**
  * format_char - formats character
- * @separator: the string seprator
+ * @separator: the string separator
  * @ap: argument pointer
 */
 void format_char(char *separator, va_list ap)
@@ -12,7 +12,7 @@ void format_char(char *separator, va_list ap)
 
 /**
  * format_int - formats integer
- * @separator: the string seprator
+ * @separator: the string separator
  * @ap: argument pointer
 */
 void format_int(char *separator, va_list ap)
@@ -22,7 +22,7 @@ void format_int(char *separator, va_list ap)
 
 /**
  * format_float - formats float
- * @separator: the string seprator
+ * @separator: the string separator
  * @ap: argument pointer
 */
 void format_string(char *separator, va_list ap)
@@ -31,7 +31,7 @@ void format_string(char *separator, va_list ap)
 }
 /**
  * format_string - formats string
- * @separator: the string seprator
+ * @separator: the string separator
  * @ap: argument pointer
 */
 void format_string(char *separator, va_list ap)
